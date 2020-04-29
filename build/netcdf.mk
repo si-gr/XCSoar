838e27f76787a631864f9eefd91d0d988bec4570
Fabian Bartschke <fabian@bartschke.de>
2020-04-29 08:48:01 +0200

    netcdf: add cross build for android

--- build/netcdf.mk
+++ build/netcdf.mk
@@ -1,7 +1,10 @@
 NETCDF = y
 
-ifeq ($(TARGET),UNIX)
+ifneq ($(TARGET),ANDROID)
 $(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
 $(eval $(call link-library,netcdfcpp,NETCDF))
+NETCDF_LDLIBS = -lnetcdf_c++4 -lnetcdf
+else
+NETCDF_LDLIBS += -l:libnetcdf_c++.a -l:libnetcdf.a
 endif
 LDLIBS += $(NETCDF_LDLIBS)
