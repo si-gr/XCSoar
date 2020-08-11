7113d904a780d2adcb31a699cbde889f9bea2876
krankenreif <krankenreif@gmail.com>
2020-08-11 22:29:28 +0200

    Remove redundant code

--- src/Weather/Skysight/CDFDecoder.cpp
+++ src/Weather/Skysight/CDFDecoder.cpp
@@ -80,22 +80,36 @@ bool CDFDecoder::DecodeSuccess() {
   return true;
 }
 
-#ifndef ANDROID
 bool CDFDecoder::Decode() {
+
+#ifndef ANDROID
   netCDF::NcFile data_file(path.c_str(), netCDF::NcFile::read);
   if(data_file.isNull()) {
     return DecodeError();
   }
   size_t lat_size = data_file.getDim("lat").getSize();
   size_t lon_size = data_file.getDim("lon").getSize();
+#else
+  NcFile data_file(path.c_str(), NcFile::FileMode::ReadOnly);
+  if(!data_file.is_valid()) {
+    return DecodeError();
+  }
+  size_t lat_size = data_file.get_dim("lat")->size();
+  size_t lon_size = data_file.get_dim("lon")->size();
+#endif
 
   // Don't use a multidmensional dynamic array for netCDF4. Allocated memory needs to be contiguous.
   double *lat_vals = new double[lat_size];
   double *lon_vals = new double[lon_size];
   double *var_vals = new double[lat_size * lon_size];
 
+#ifndef ANDROID
   data_file.getVar("lat").getVar(lat_vals);
   data_file.getVar("lon").getVar(lon_vals);
+#else
+  data_file.get_var("lat")->get(&lat_vals[0], lat_size);
+  data_file.get_var("lon")->get(&lon_vals[0], lon_size);
+#endif
 
   double lat_min = lat_vals[lat_size - 1];
   double lat_max = lat_vals[0];
@@ -103,23 +117,33 @@ bool CDFDecoder::Decode() {
   double lon_max = lon_vals[lon_size - 1];
   double lon_scale = (lon_max - lon_min)/lon_size;
   double lat_scale = (lat_max - lat_min)/lat_size;
-  
-
 
   delete [] lat_vals;
   delete [] lon_vals;
+
+#ifndef ANDROID
   netCDF::NcVar data_var = data_file.getVar(data_varname);
   if(data_var.isNull()) {
+    delete [] var_vals;
     return DecodeError();
   }
-
   data_var.getVar(var_vals);
-
   double fill_value;
   float var_offset, var_scale;
   data_var.getAtt("_FillValue").getValues(&fill_value);
   data_var.getAtt("add_offset").getValues(&var_offset);
   data_var.getAtt("scale_factor").getValues(&var_scale);
+#else
+  NcVar *data_var = data_file.get_var(data_varname.c_str());
+  if(!data_var->is_valid()) {
+    delete [] var_vals;
+    return DecodeError();
+  }
+  data_var->get(var_vals, (long)lat_size, (long)lon_size);
+  double fill_value = data_var->get_att("_FillValue")->values()->as_double(0);
+  float var_offset = data_var->get_att("add_offset")->values()->as_float(0);
+  float var_scale = data_var->get_att("scale_factor")->values()->as_float(0);
+#endif
 
   //Generate GeoTiff
   TIFF *tf = XTIFFOpen(output_path.c_str(), "w");
@@ -171,9 +195,9 @@ bool CDFDecoder::Decode() {
   int index = 0;
   bool success = true;
   double point_value;
-  for(int y=(int)lat_size-1; y>=0; y--) {
+  for(int y = (int)lat_size-1; y >= 0; y--) {
     memset(row, 0, linebytes * sizeof(unsigned char)); //ensures unused data points are transparent
-    for(int x=0;x<(int)lon_size;x++) {
+    for(int x = 0; x < (int)lon_size; x++) {
       index = (y * lon_size) + x;
       rb = x * samplesperpixel;
       if(var_vals[index] != fill_value) {
@@ -209,133 +233,6 @@ bool CDFDecoder::Decode() {
   File::Delete(path);
   return (success) ? DecodeSuccess() : DecodeError();
 }
-#else
-bool CDFDecoder::Decode() {
-  NcFile data_file(path.c_str(), NcFile::FileMode::ReadOnly);
-  
-  if(!data_file.is_valid()) {
-    return DecodeError();
-  }
-  size_t lat_size = data_file.get_dim("lat")->size();
-  size_t lon_size = data_file.get_dim("lon")->size();
-  //return DecodeError();
-  
-  double *lat_vals = new double[lat_size];
-  double *lon_vals = new double[lon_size];
-  double *var_vals = new double[lat_size * lon_size];
-
-  data_file.get_var("lat")->get(&lat_vals[0], lat_size);
-  data_file.get_var("lon")->get(&lon_vals[0], lon_size);
-
-  double lat_min = lat_vals[lat_size - 1];
-  double lat_max = lat_vals[0];
-  double lon_min = lon_vals[0];
-  double lon_max = lon_vals[lon_size - 1];
-  double lon_scale = (lon_max - lon_min)/lon_size;
-  double lat_scale = (lat_max - lat_min)/lat_size;
-  delete [] lat_vals;
-  delete [] lon_vals;
-  NcVar *data_var = data_file.get_var(data_varname.c_str());
-  if(!data_var->is_valid()) {
-    delete [] var_vals;
-    return DecodeError();
-  }
-
-  data_var->get(var_vals, (long)lat_size, (long)lon_size);
-  double fill_value = data_var->get_att("_FillValue")->values()->as_double(0);
-  float var_offset = data_var->get_att("add_offset")->values()->as_float(0);
-  float var_scale = data_var->get_att("scale_factor")->values()->as_float(0);
-
-  TIFF *tf = XTIFFOpen(output_path.c_str(), "w");
-  if(!tf) {
-    delete [] var_vals;
-    return DecodeError();
-  }
-  
-  GTIF *gt = GTIFNew(tf);
-  if(!gt) {
-    (void)TIFFClose(tf);
-    delete [] var_vals;
-    return DecodeError();
-  }
-  double tp_topleft[6] = {0,0,0,lon_min, lat_min, 0}; 
-  double pix_scale[3]={lon_scale,-lat_scale,0}; 
-
-
-  const int samplesperpixel = 4;
-  TIFFSetField(tf, TIFFTAG_IMAGEWIDTH, lon_size);
-  TIFFSetField(tf, TIFFTAG_IMAGELENGTH, lat_size); 
-  TIFFSetField(tf, TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
-  TIFFSetField(tf, TIFFTAG_BITSPERSAMPLE, 8); 
-  TIFFSetField(tf, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
-  TIFFSetField(tf, TIFFTAG_COMPRESSION, COMPRESSION_DEFLATE);
-  TIFFSetField(tf, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-  TIFFSetField(tf, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
-
-  TIFFSetField(tf, TIFFTAG_GEOTIEPOINTS, 6, tp_topleft);
-  TIFFSetField(tf,TIFFTAG_GEOPIXELSCALE, 3,pix_scale);
-  
-  GTIFKeySet(gt, GTModelTypeGeoKey, TYPE_SHORT, 1, ModelTypeGeographic);
-  GTIFKeySet(gt, GTRasterTypeGeoKey, TYPE_SHORT,  1, RasterPixelIsArea); 
-  GTIFKeySet(gt, GeographicTypeGeoKey, TYPE_SHORT,  1, GCS_WGS_84); 
-  GTIFKeySet(gt, GTCitationGeoKey, TYPE_ASCII,  27, "Generated by XCSoar with data from skysight.io"); 
-  GTIFKeySet(gt, GeogLinearUnitsGeoKey, TYPE_SHORT,  1, Linear_Meter); 	
-  GTIFKeySet(gt, GeogAngularUnitsGeoKey, TYPE_SHORT,  1, Angular_Degree); 
-
-  
-
-  tsize_t linebytes = samplesperpixel * lon_size;
-  unsigned char *row;
-
-  row = (TIFFScanlineSize(tf) > linebytes) ?
-    (unsigned char *)_TIFFmalloc(linebytes) :
-    (unsigned char *)_TIFFmalloc(TIFFScanlineSize(tf));
-  TIFFSetField(tf, TIFFTAG_ROWSPERSTRIP, TIFFDefaultStripSize(tf, linebytes));
-
-  int rb;
-  bool success = true;
-  double point_value;
-  for(int y = (int)lat_size - 1; y >= 0; y--) {
-    memset(row, 0, linebytes * sizeof(unsigned char)); //ensures unused data points are transparent
-    for(int x = 0; x < (int)lon_size; x++) {
-      rb = x * samplesperpixel;
-      if(var_vals[y * (int)lon_size + x] != fill_value) {
-        point_value = (var_vals[y * (int)lon_size + x] * var_scale) + var_offset;
-        if(point_value > legend.begin()->first) {
-          auto color_index = legend.lower_bound(point_value);
-          color_index--;
-
-          row[rb] = color_index->second.Red;
-          row[rb+1] = color_index->second.Green;
-          row[rb+2] = color_index->second.Blue;
-          row[rb+3] = (unsigned char)255;
-        } 
-      } 
-    } 
-
-    if (TIFFWriteScanline(tf, row, (uint32_t)(lat_size-(y+1)), 0) != 1) {
-      success = false;
-      break;
-    }
-  }
-  delete [] var_vals;
-  
-  if(success)
-      GTIFWriteKeys(gt);
-      
-  (void)TIFFClose(tf);
-  
-  if (row)
-    _TIFFfree(row);
-  
-  GTIFFree(gt);
-
-  data_file.close();
-  File::Delete(path);
-  return (success) ? DecodeSuccess() : DecodeError();
-}
-
-#endif
 
 void CDFDecoder::MakeCallback(bool result) {
   
