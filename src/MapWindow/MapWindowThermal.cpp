ac703af5015fc3311c7cf40102de84cb5bfdca22
simon.grafenhorst <simon.grafenhorst@kit.edu>
2026-04-11 16:27:38 +0200

    Color thermals depending on strength (Opencode)

--- src/MapWindow/MapWindowThermal.cpp
+++ src/MapWindow/MapWindowThermal.cpp
@@ -4,6 +4,10 @@
 #include "MapWindow.hpp"
 #include "Look/MapLook.hpp"
 #include "ui/canvas/Icon.hpp"
+#include "ui/canvas/Brush.hpp"
+#include "ui/canvas/Pen.hpp"
+#include "Screen/Layout.hpp"
+#include "Renderer/TextInBox.hpp"
 #ifdef HAVE_SKYLINES_TRACKING
 #include "Tracking/SkyLines/Data.hpp"
 #endif
@@ -43,7 +47,7 @@ MapWindow::DrawThermalEstimate(Canvas &canvas) const noexcept
   const DerivedInfo &calculated = Calculated();
   const ThermalLocatorInfo &thermal_locator = calculated.thermal_locator;
 
-  if (render_projection.GetMapScale() > 4000)
+  if (render_projection.GetMapScale() > 10000)
     return;
 
   // draw only at close map scales in non-circling mode
@@ -67,10 +71,39 @@ MapWindow::DrawThermalEstimate(Canvas &canvas) const noexcept
 
 #ifdef HAVE_HTTP
   if (tim_glue != nullptr && GetComputerSettings().weather.enable_tim) {
+    const uint8_t color_wheel[10][3] = {
+      {0, 0, 255},
+      {0, 113, 255},
+      {0, 227, 255},
+      {0, 255, 170},
+      {0, 255, 57},
+      {57, 255, 0},
+      {170, 255, 0},
+      {255, 227, 0},
+      {255, 113, 0},
+      {255, 0, 0}};
+
     const auto lock = tim_glue->Lock();
-    for (const auto &i : tim_glue->Get())
-      if (auto p = render_projection.GeoToScreenIfVisible(i.location))
-        look.thermal_source_icon.Draw(canvas, *p);
+    for (const auto &i : tim_glue->Get()) {
+      if (auto p = render_projection.GeoToScreenIfVisible(i.location)) {
+        const auto diff = std::chrono::system_clock::now() - i.time;
+        auto color_i = std::min(std::max(int(i.climb_rate / 0.5f), 0), 9);
+        auto radius = std::max(10 - int(std::chrono::round<std::chrono::seconds>(diff).count() / 200), 1);
+
+        canvas.SelectHollowBrush();
+        canvas.Select(Pen(Layout::Scale(2),
+                          Color(color_wheel[color_i][0], color_wheel[color_i][1], color_wheel[color_i][2])));
+        canvas.DrawCircle(*p, Layout::Scale(radius));
+
+        char thermal_text[32];
+        StringFormat(thermal_text, sizeof(thermal_text), "%.1f", i.climb_rate);
+        PixelPoint text_point = *p;
+        text_point.y -= Layout::Scale(5);
+        text_point.x -= Layout::Scale(5);
+        TextInBoxMode mode;
+        TextInBox(canvas, thermal_text, text_point, mode, GetClientRect());
+      }
+    }
   }
 #endif
 }
